f3a280e805a56124500637ecc504e522661132c6
IvanHapo <ivanhaponouk@gmail.com>
2022-04-17 21:56:34 -0300

    Desafio Clase Eventos

--- src/componentes/ItemDetail/ItemDetail.js
+++ src/componentes/ItemDetail/ItemDetail.js
@@ -1,30 +1,101 @@
 import './ItemDetail.css'
+import { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
 
-const ItemDetail = ({ nombre, img, descripcion, precio,categoria }) => {
+const InputCount = ({onConfirm, stock, initial=1}) => {
+    const [count, setCount] = useState(initial)
+    console.log(stock)
+    const handleChange = (e) => {
+        if(e.target.value <= stock) {
+            setCount(e.target.value)
+        }
+    }
+
     return (
-        <article className="card-contenedor">
-            <header className="">
-                <h2 className="">
+        <div>
+            <input type='number' onChange={handleChange} value={count}/>
+            <button onClick={() => onConfirm(count)}>Agregar al carrito</button>
+        </div>
+    )
+}
+
+const ButtonCount = ({ onConfirm, stock, initial = 1 }) => {
+    const [count, setCount] = useState(initial)
+
+    const increment = () => {
+        if(count < stock)
+            setCount(count + 1)
+
+    }
+
+    const decrement = () => {
+        if(count > initial)
+            setCount(count - 1)
+
+    }
+
+    return (
+        <div>
+            <p>{count}</p>
+            <button onClick={decrement}>-</button>
+            <button onClick={increment}>+</button>
+            <button onClick={() => onConfirm(count)}>Agregar al carrito</button>
+        </div>
+    )
+}
+
+const Select = ({ options = [], onSelect}) => {
+    return (
+        <select onChange={(e) => onSelect(e.target.value)}>
+            {options.map(o => <option key={o.id} value={o.value}>{o.text}</option>)}
+        </select>
+    )
+}
+
+const ItemDetail = ({ id, nombre, img, categoria, descripcion, precio, stock }) => {
+    const [typeInput, setTypeInput] = useState(true)
+    const [quantity, setQuantity] = useState(0) 
+    const options = [{id: 0, value: '', text: '-'}, {id: 1, value: '/', text: 'ItemListContainer'}, {id: 2, value: '/form', text: 'Formulario'}]
+    const navigate = useNavigate()
+
+    const handleAdd = (count) => {
+        console.log('Agregar al carrito')
+        setQuantity(count)
+    }
+
+    const handleSelect = (value) => {
+        navigate(value)
+    }
+
+    const Count = typeInput ? ButtonCount : InputCount
+
+    return (
+        <div className="card-contenedor">
+            <header className="Header">
+                <button onClick={() => setTypeInput(!typeInput)}>Cambiar Count</button>
+                <h2 className="ItemHeader">
                     {nombre}
                 </h2>
             </header>
             <picture>
-                <img src={img} alt={nombre} className=""/>
+                <img src={img} alt={nombre} className="ItemImg"/>
             </picture>
-            <section>
-                <p className="">
+            <div>
+                <p>
                     Categoria: {categoria}
                 </p>
                 <p className="text-description">
                     Descripción: {descripcion}
                 </p>
-                <p className="">
+                <p >
                     Precio: {precio}
                 </p>
-            </section>           
-            <footer className=''>
+            </div>           
+            <footer>
+                <Select options={options} onSelect={handleSelect} />
+                {quantity > 0 ? <Link to='/cart'>Ir al carrito</Link> : <Count onConfirm={handleAdd} stock={stock}/> } 
             </footer>
-        </article>
+        </div>
     )
 }
 
